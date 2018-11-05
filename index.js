76d45e2b9ce753a133aad7e08ea0d1e31eb94766
Andrew <andrew@clearpathdevelopment.com>
2018-11-05 11:11:16 +0700

    Add website

--- index.js
+++ index.js
@@ -8,7 +8,81 @@ let username = 'daicaca';
 let password = 'xxxxxxx';
 let globalCookie = '';
 
-main();
+
+var express = require('express')
+var app = express()
+app.use('/website', express.static('website'))
+
+//hyipUrl, username , pass, hourly, time to withdraw, login url (option)
+var accounts = []
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/listaccount', function (req, res) {
+   res.setHeader('Content-Type', 'application/json');
+   res.send(JSON.stringify(accounts));
+})
+
+app.get('/saveaccount', function (req, res) {
+	var hyip = {};
+	if(req.query.hyipurl) hyip.hyipUrl = req.query.hyipurl;
+	else hyip.hyipUrl = '';
+	if(req.query.username) hyip.username = req.query.username;
+	else hyip.username = '';
+	if(req.query.password) hyip.password = req.query.password;
+	else hyip.password = '';
+	if(req.query.hourly) hyip.hourly = req.query.hourly;
+	else hyip.hourly = 0;
+	if(req.query.time) hyip.time = req.query.time;
+	else hyip.time = 0;
+	if(req.query.loginurl) hyip.loginUrl = req.query.loginurl;
+	else hyip.loginUrl = '';
+	var count = 0;
+	for(count = 0; count< accounts.length; count++)
+	{
+		if(accounts[count].hyipUrl == hyip.hyipUrl)
+		{
+			accounts[count] = hyip;
+			break;
+		}
+	}
+	
+	if(accounts.length == count)
+	{
+		accounts.push(hyip);
+	}
+	
+   res.setHeader('Content-Type', 'application/json');
+   res.send(JSON.stringify(hyip));
+})
+
+app.get('/deleteaccount', function (req, res) {
+	var hyip = {};
+	if(req.query.hyipurl)
+	{
+		for(var count = 0; count< accounts.length; count++)
+		{
+			if(accounts[count].hyipUrl == req.query.hyipurl)
+			{
+				hyip = accounts[count];
+				break;
+			}
+		}
+	}
+	if(accounts.length > count)
+	{
+		accounts.splice(count, 1);
+	}
+	
+   res.setHeader('Content-Type', 'application/json');
+   res.send(JSON.stringify(hyip));
+})
+
+app.listen(3000)
+
+
+//main();
 
 function main() {
 	request(baseUrl + '/?a=login', function (error, response, body) {
