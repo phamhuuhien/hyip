8c8435c6f04df12aa3c950d115d65af61e07181f
Hien Pham <tranvanthien@github.com>
2018-11-08 07:57:03 +0700

    using lokijs db

--- index.js
+++ index.js
@@ -1,88 +1,77 @@
 var express = require('express');
 var cron = require('node-cron');
+var loki = require('lokijs');
 const { autoWithDraw } = require('./utils/autowithdraw.js');
 
 var app = express()
 app.use('/website', express.static('website'))
 
-//hyipUrl, username , pass, hourly, time to withdraw, login url (option)
-var accounts = []
+var db = new loki('loki.json')
+var accounts = db.addCollection('accounts')
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+	res.send('Hello World')
 })
 
 app.get('/listaccount', function (req, res) {
-   res.setHeader('Content-Type', 'application/json');
-   res.send(JSON.stringify(accounts));
+	res.setHeader('Content-Type', 'application/json');
+	console.log("accounts.find()", accounts.find());
+	res.send(JSON.stringify(accounts.find()));
 })
 
 app.get('/saveaccount', function (req, res) {
 	var hyip = {};
-	if(req.query.hyipurl) hyip.hyipUrl = req.query.hyipurl;
+	if (req.query.hyipurl) hyip.hyipUrl = req.query.hyipurl;
 	else hyip.hyipUrl = '';
-	if(req.query.username) hyip.username = req.query.username;
+	if (req.query.username) hyip.username = req.query.username;
 	else hyip.username = '';
-	if(req.query.password) hyip.password = req.query.password;
+	if (req.query.password) hyip.password = req.query.password;
 	else hyip.password = '';
-	if(req.query.email) hyip.email = req.query.email;
+	if (req.query.email) hyip.email = req.query.email;
 	else hyip.email = 0;
-	if(req.query.time) hyip.time = req.query.time;
-	else hyip.time = new Date().getMilliseconds() + 60*60*1000;
-	if(req.query.loginurl) hyip.loginUrl = req.query.loginurl;
+	if (req.query.time) hyip.time = req.query.time;
+	else hyip.time = new Date().getMilliseconds() + 60 * 60 * 1000;
+	if (req.query.loginurl) hyip.loginUrl = req.query.loginurl;
 	else hyip.loginUrl = '';
-	var count = 0;
-	for(count = 0; count< accounts.length; count++)
-	{
-		if(accounts[count].hyipUrl == hyip.hyipUrl && accounts[count].username == hyip.username)
-		{
-			accounts[count] = hyip;
-			break;
-		}
+
+	let editing = accounts.find({ 'hyipUrl': hyip.hyipUrl, 'username': hyip.username });
+	if (editing.length > 0) {
+		return;
 	}
-	
-	if(accounts.length == count)
-	{
-		accounts.push(hyip);
-	}
-	
-   res.setHeader('Content-Type', 'application/json');
-   res.send(JSON.stringify(hyip));
+
+	accounts.insert(hyip);
+
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(hyip));
 })
 
 app.get('/deleteaccount', function (req, res) {
-	var hyip = {};
-	if(req.query.hyipurl)
-	{
-		for(var count = 0; count< accounts.length; count++)
-		{
-			if(accounts[count].hyipUrl == req.query.hyipurl && accounts[count].username == hyip.usernam)
-			{
-				hyip = accounts[count];
-				break;
-			}
-		}
-	}
-	if(accounts.length > count)
-	{
-		accounts.splice(count, 1);
-	}
-	
-   res.setHeader('Content-Type', 'application/json');
-   res.send(JSON.stringify(hyip));
+	if (!req.query.hyipurl)
+		return;
+
+	let hyip = accounts.find({ 'hyipUrl': req.query.hyipurl, 'username': req.query.username });
+	accounts.remove(hyip);
+
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(hyip[0]));
 })
 
 app.listen(80);
 console.log("Server start: port 80");
 
 cron.schedule('*/5 * * * *', async () => {
-	let accountsProcessing = accounts.slice(0);
+	let accountsProcessing = accounts.find();
 	console.log("account length", accountsProcessing.length);
 	await accountsProcessing.forEach(account => {
 		let currrentTime = new Date().getMilliseconds();
 		//if (account.time < currrentTime) {
-			console.log(`withdraw account ${account.username} from ${account.hyipUrl}`);
+		console.log(`withdraw account ${account.username} from ${account.hyipUrl}`);
+		try {
 			autoWithDraw(account);
-			//account.time = currrentTime + 60*60*1000;
+		} catch (error) {
+			console.log("error", error);
+		}
+		//account.time = currrentTime + 60*60*1000;
 		//}
 	});
 });
--- website/js/app.js
+++ website/js/app.js
@@ -1,61 +1,61 @@
 angular.module('myApp', [])
-.controller("listAccountControl", ['$scope', '$http', function ($scope, $http){
-	
-	$scope.accounts = []
-	$scope.getList = function(){
-		$http({
-			  method: 'GET',
-			  url: '/listaccount'
+	.controller("listAccountControl", ['$scope', '$http', function ($scope, $http) {
+
+		$scope.accounts = []
+		$scope.getList = function () {
+			$http({
+				method: 'GET',
+				url: '/listaccount'
 			}).then(function successCallback(response) {
-				if(response && response.data){
+				if (response && response.data) {
 					$scope.accounts = response.data;
 				}
-				else{
+				else {
 					alert('Error when getting accounts');
 				}
-			  }, function errorCallback(response) {
-				  alert('Error when get accounts');
-			  });
-	}
-	$scope.getList();
-	
-	$scope.save = function(account){
-		$http({
-			  method: 'GET',
-			  url: '/saveaccount/?hyipurl='+account.hyipUrl +'&loginurl='+account.loginUrl+'&username='+account.username +'&password='+ account.password+'&time='+ account.time+'&hourly='+account.hourly
+			}, function errorCallback(response) {
+				alert('Error when get accounts');
+			});
+		}
+		$scope.getList();
+
+		$scope.save = function (account) {
+			$http({
+				method: 'GET',
+				url: '/saveaccount/?hyipurl=' + account.hyipUrl + '&loginurl=' + account.loginUrl + '&username=' + account.username + '&password=' + account.password + '&time=' + account.time + '&email=' + account.email
 			}).then(function successCallback(response) {
-				if(response && response.data && response.data.hyipUrl){
+				if (response && response.data && response.data.hyipUrl) {
 					alert('Saved successfully');
 				}
-				else{
+				else {
 					alert('Error when saving account');
 				}
-			  }, function errorCallback(response) {
-				  alert('Error when saving account');
-			  });
-	}
-	
-	$scope.add = function()
-	{
-		$scope.accounts.push({});
-	}
-	
-	$scope.delete = function(hyipurl){
-		if(hyipurl && confirm('Do you want to delete: '+ hyipurl)){
-			$http({
-				  method: 'GET',
-				  url: '/deleteaccount/?hyipurl='+hyipurl
+			}, function errorCallback(response) {
+				console.log("response", response);
+				alert('Error when saving account');
+			});
+		}
+
+		$scope.add = function () {
+			$scope.accounts.push({});
+		}
+
+		$scope.delete = function (hyip) {
+			if (hyip.hyipUrl && confirm('Do you want to delete: ' + hyip.hyipUrl)) {
+				$http({
+					method: 'GET',
+					url: '/deleteaccount/?hyipurl=' + hyip.hyipUrl + '&username=' + hyip.username,
 				}).then(function successCallback(response) {
-					if(response && response.data && response.data.hyipUrl){
+					if (response && response.data && response.data.hyipUrl) {
 						alert('Deleted successfully');
 						$scope.getList();
 					}
-					else{
+					else {
 						alert('Error when deleting account');
 					}
-				  }, function errorCallback(response) {
-					  alert('Error when deleting account');
-				  });
+				}, function errorCallback(response) {
+					alert('Error when deleting account');
+				});
+			}
 		}
-	}
-}]);
+	}]);
